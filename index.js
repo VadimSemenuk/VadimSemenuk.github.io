b0405712a7c8a3af97e4d69a626a3a47553a889b
Vadim Semenyuk <vadim54787@gmail.com>
2025-03-21 01:53:33 +0300

    init

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register("/serviceworker.js");
+run()
+
+async function run() {
+    const res = await Notification.requestPermission()
+    if (res === "granted") {
+        if ('serviceWorker' in navigator) {
+            navigator.serviceWorker.register("/serviceworker.js")
+                .then(registration => {
+                    registration.showNotification('Hello!', {
+                        body: 'This is a notification example.',
+                        icon: 'icon.png' // Replace with your notification icon
+                    });
+                });
+        }
+    }
 }
--- serviceworker.js
+++ serviceworker.js
@@ -1,15 +1,9 @@
 self.addEventListener('install', (event) => {
-    // предотвращает завершение события до тех пор, пока не завершится асинхронная операция(используется, чтобы Service Worker не был деактивирован браузером)
-    event.waitUntil((async () => {
-        // берем объект кэша и добавляем туда наши файлы
-        // const cache = await caches.open(CACHE_NAME);
-        // await cache.add('fallback.html');
+    console.log(1)
 
-    })());
-
-    self.registration.showNotification("ttitle", {
-        body: "messageee",
-        silent: false,
-        tag: 'ABRDirect',
-    });
+    // self.registration.showNotification("ttitle", {
+    //   body: "messageee",
+    //   silent: false,
+    //   tag: 'ABRDirect',
+    // });
 });
