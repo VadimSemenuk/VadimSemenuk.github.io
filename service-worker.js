f13f45c415e47430b89a6ec358d75fa6c1e3fdc7
Vadim Semenyuk <vadim54787@gmail.com>
2025-07-18 21:36:06 +0300

    1

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+self.addEventListener('install', (event) => {
+    console.log('Установлен');
+});
+
+self.addEventListener('activate', (event) => {
+    console.log('Активирован');
+});
+
+self.addEventListener('push', function(event) {
+    console.log('push', event);
+    // const data = event.data.json();
+    // const options = {
+    //     body: data.message,
+    //     // icon: 'icons/icon-72x72.png',
+    //     // vibrate: [100, 50, 100],
+    // };
+    // event.waitUntil(
+    //     self.registration.showNotification(data.title, options)
+    // );
+});
